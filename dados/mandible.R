20afd82d8ff652558edbc89d8c45f42f41b4b6d0
LEG UFPR <leg.ufpr@gmail.com>
2019-09-04 23:16:28 +0000

    The site is alive.

---
+++ dados/mandible.R
@@ -0,0 +1,5 @@
+"mandible" <-
+structure(list(females = c(120, 107, 110, 116, 114, 111, 113, 
+117, 114, 112), male = c(110, 111, 107, 108, 110, 105, 107, 106, 
+111, 111)), .Names = c("females", "male"), row.names = c("1", 
+"2", "3", "4", "5", "6", "7", "8", "9", "10"), class = "data.frame")
