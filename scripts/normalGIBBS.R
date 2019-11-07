c598f88f6e6e2104db329a817a44da9ce2f86c17
Fernando Mayer <fernandomayer@gmail.com>
2019-11-06 23:05:08 -0300

    Fix typo

--- scripts/normalGIBBS.R
+++ scripts/normalGIBBS.R
@@ -14,10 +14,10 @@
 ## Marginais
 ##    [\mu|y] \sim {\rm t}_{n-1}(\overline{y}, SQ/n)
 ##    \frac{\mu - \overline{y}}{\sqrt{sigma^2/n}} \sim {\rm t}_{n-1}
-##    
+##
 ##    [\sigma^2|y] \sim {\rm IG}(\frac{n-1}{2}, \frac{2}{SQ})
 ##    \frac{SQ}{\sigma^2} \sim \chi^2_{n-1}
-##    
+##
 
 set.seed(20180419)
 (y <- rnorm(12, mean=50, sd=8))
@@ -26,9 +26,9 @@ dados <- list(n=length(y), m=mean(y), v = var(y), SQ = sum((y-mean(y))^2))
 ## Amostra (exata) da posteriori
 ##
 ## para amostrar de pode-se explorar a fatoração:
-## [\mu, \sigma^2|y] = [\sigma^2|y] \cdot [\mu|\sigma^2,y] = 
+## [\mu, \sigma^2|y] = [\sigma^2|y] \cdot [\mu|\sigma^2,y] =
 ## ou, alternativamente
-## [\mu, \sigma^2|y] = [\mu|y] \cdot [\sigma^2|\mu,y] = 
+## [\mu, \sigma^2|y] = [\mu|y] \cdot [\sigma^2|\mu,y] =
 ##
 ## Vamos adotar aqui a primeira fatoração:
 ## Obtendo uma amostra
@@ -57,11 +57,11 @@ lines(density(chi.sim), col=4)
 ## Amostra (Gibbs) da posteriori
 ##
 ## A estatégia de Gibbs é alternar as simulações entre **as distribuições condicionais**
-## o que "parece" errado ,as provouse que a cadeia de valores assim simulados **converge** para a distribuição conjunta 
+## o que "parece" errado ,as provouse que a cadeia de valores assim simulados **converge** para a distribuição conjunta
 ##    [\mu|\sigma^2, y] \sim {\rm N}(\overline{y}, \sigma^2/n)
 ##    [\sigma^2|\mu, y] \sim {\rm IG}(\frac{n}{2}, \frac{2}{A})
 ## Obtendo uma amostra
-## Como a distribuição de um parâmetro depende da distribuição do outro, 
+## Como a distribuição de um parâmetro depende da distribuição do outro,
 ## é necessário fornecer/arbitrar um valor para inicial o algoritmo
 mu0 <- 50
 ##  (i) Amostrar \sigma^2 de [\sigma^2|\mu, y]
@@ -111,7 +111,7 @@ lines(density(chi.simG), col=3, lwd=2)
 
 
 ## Relação entre Chi-quadrado escalonada inversa e Gamma
-a1 <- dados$SQ/rchisq(10000), df=24)
+a1 <- dados$SQ/rchisq(10000, df=24)
 a2 <- 1/rgamma(1000, shape=(dados$n-1)/2, scale=2/dados$SQ)
 plot(density(a1))
 lines(density(a2), col=2)
